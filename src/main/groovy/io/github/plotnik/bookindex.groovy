06a0ee31e3348a1d18e20195657c0dc6a8829784
plotnik <egor.abramovich@gmail.com>
2018-03-26 23:20:21 +0300

    first commit

---
+++ src/main/groovy/io/github/plotnik/bookindex.groovy
@@ -0,0 +1,7 @@
+package io.github.plotnik
+
+println "-- bookindex"
+
+// http://mvnrepository.com
+// https://github.com/a-services/apperyunit/blob/master/src/main/groovy/io/appery/apperyunit/DashboardFrame.java
+// https://docs.oracle.com/javase/tutorial/uiswing/components/filechooser.html
